de8c3b727e6a1bc13ea7c4cdd04d1766709c40dc
letty-Rock <leticiasantos.roque@outlook.com>
2021-07-08 16:41:37 -0300

    Atualizações

--- tamplate etc/arrays-spread-metodos.js
+++ tamplate etc/arrays-spread-metodos.js
@@ -16,3 +16,19 @@ console.log(allFriends)
 // Quando precisamos de um retorno qu enão modifique o array anterios o map é perfeito
 
 allFriends.map(Friends => console.log(Friends))
+
+//Filter + array
+
+const numeros = [24, 58, 49, 22, 18, 45]
+const numerosImpares = numeros.filter(numero => numero % 2 != 0)
+
+console.log(numerosImpares)
+const numerosPares = numeros.filter(numero => numero % 2 == 0)
+console.log(numerosPares)
+//numeros ordenados
+
+const numerosOrdenadosCrescente = numeros.sort((a, b) => a + b)
+console.log(numerosOrdenadosCrescente)
+
+const numerosOrdenadosDecrescente = numero.sort((a, b) => b - a)
+console.log(numerosOrdenadosDecrescente)
